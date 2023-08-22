d1e1afde43d1aec56fea5410569fb77bdb5add05
Tom Bartuzel <tom.bartuzel@epitech.eu>
2023-08-22 10:42:11 -0700

    feat(map): interact with the markers DONE
    need to print only today's delivery, maybe add the possibility to change the date

--- lib/views/home/map/map.dart
+++ lib/views/home/map/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 
@@ -20,7 +21,7 @@ class _MapPageState extends State<MapPage> {
   List<Polyline> polylines = [];
   List<Marker> markerCoord = [];
   List<LatLng> coordinates = [];
-  List<LatLng> markerCoordinates = [];
+  LatLng markerCoordinates = LatLng(0, 0);
   double long = 0;
   double lat = 0;
   double lastLat = 0;
@@ -35,21 +36,57 @@ class _MapPageState extends State<MapPage> {
   late DateTime todayDate;
   late DateTime todayStart;
   late DateTime todayEnd;
-  LatLng coord = LatLng(0, 0);
   int userId = -1;
   bool deliveryToday = true;
 
+  void _showMarkerInfo(String address, String begin, String end) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delivery Info'),
+          content: RichText(
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: <TextSpan>[
+                const TextSpan(
+                  text: 'Address: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: '$address \n'),
+                const TextSpan(
+                  text: 'Start Time: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: '$begin\n'),
+                const TextSpan(
+                  text: 'End Time: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: '$end'),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
-    // Random random = Random();
-    todayDate =
-        DateTime.now(); // Use DateTime.now() to get the current date and time.
+    todayDate = DateTime.now();
     todayStart = DateTime(todayDate.year, todayDate.month, todayDate.day, 1);
     todayEnd = DateTime(todayDate.year, todayDate.month, todayDate.day, 23);
-    userId = _api.user!
-        .getUserId(); // Replace this with your actual way of getting the user ID.
+    userId = _api.user!.getUserId();
 
-    // Make sure _api.mapValues returns a Future<String>.
     Future<String> allTodaysTrajValues =
         _api.mapValues(todayStart, todayEnd, userId);
 
@@ -59,30 +96,40 @@ class _MapPageState extends State<MapPage> {
       // CHECK OUT LIGNE 65: dynamic traj = responseData[i]['path'][0]['path']; -> [0] MIGHT NEED TO BE IN A LOOP TO GET ALL THE TRAJ OF THE DAY AND NOT JUST THE FIRST ONE
       for (int i = 0; i < responseData.length; i++) {
         coordinates = [];
-        dynamic trajId = responseData[i]['id'];
-        print(trajId);
         dynamic traj = responseData[i]['path'][0]['path'];
-        int len = traj.length - 1;
+        dynamic stopPoints = responseData[i]['stop_point'];
         for (int a = 0; a < traj.length; a++) {
           coordinates.add(LatLng(traj[a]['lat'], traj[a]['lon']));
         }
+        for (int a = 0; a < stopPoints.length; a++) {
+          String lat = stopPoints[a]['lat'];
+          String long = stopPoints[a]['long'];
+          double doubleLat = double.parse(lat);
+          double doubleLong = double.parse(long);
+          markerCoordinates = LatLng(doubleLat, doubleLong);
+          String address = stopPoints[a]['autocompleteAdress'];
+          DateTime begining = DateTime.parse(stopPoints[a]['begin']);
+          DateTime ending = DateTime.parse(stopPoints[a]['end']);
+          DateFormat outputFormat = DateFormat('yyyy-MM-dd HH:mm');
+          String formattedBegin = outputFormat.format(begining);
+          String formattedEnd = outputFormat.format(ending);
+          markerCoord.add(Marker(
+              width: 80.0,
+              height: 80.0,
+              point: markerCoordinates,
+              builder: (ctx) => GestureDetector(
+                    onTap: () {
+                      _showMarkerInfo(address, formattedBegin, formattedEnd);
+                    },
+                    child: const Icon(
+                      Icons.location_on,
+                      color: Colors.black,
+                      size: 48,
+                    ),
+                  )));
+        }
         final color = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
             .withOpacity(1.0);
-        coord = LatLng(traj[len]['lat'], traj[len]['lon']);
-        markerCoord.add(Marker(
-            width: 80.0,
-            height: 80.0,
-            point: coord,
-            builder: (ctx) => GestureDetector(
-                  onTap: () {
-                    print('Marker clicked ${coord} ${trajId} !');
-                  },
-                  child: const Icon(
-                    Icons.location_on,
-                    color: Colors.black,
-                    size: 48,
-                  ),
-                )));
         polylines.add(Polyline(
           points: coordinates,
           color: color,
