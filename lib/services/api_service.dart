594d217f2a371af40a0d203ca53ad2920c4b1a97
Tom Bartuzel <tom.bartuzel@epitech.eu>
2023-09-25 20:34:16 -0700

    feat(login password change): change password at login

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -22,6 +22,8 @@ class ApiRoutes {
       '/user/reset-password/:token'; // POST
   static const String resetPasswordWithoutTokenRoute =
       '/user/reset-password'; // POST
+  static const String resetPasswordGenerateToken =
+      '/user/reset-password-generate';
 
   // User(s)
   static const String usersRoute = '/user'; // GET
@@ -395,7 +397,8 @@ class ApiService {
         _logger.i(responseData);
         return (users);
       } else {
-        _logger.e('patchUser failed with status code ${response.statusCode}');
+        _logger.e(
+            'patchUser failed with status code ${response.statusCode}: ${response.body}');
         return (null);
       }
     } catch (e) {
@@ -436,7 +439,50 @@ class ApiService {
         _logger.i(responseData);
         return true;
       } else {
-        _logger.e('Login failed with status code ${response.statusCode}');
+        _logger.e(
+            'Login failed with status code ${response.statusCode}: ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      _logger.e(e.toString());
+      return false;
+    }
+  }
+
+  /// Permet changer de mot de passe.
+  ///
+  /// Prend en paramètre un [email].
+  ///
+  /// Peut prendre en paramètre un [client] qui est un [Client]
+  ///
+  /// Retourne un [bool] qui indique si la connexion a réussi.
+  Future<bool> generatePasswordToken(
+    String email, {
+    Client? client,
+  }) async {
+    try {
+      client ??= _httpClient;
+      Uri url = ApiRoutes.route(ApiRoutes.resetPasswordGenerateToken);
+      Response response = await client.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+          },
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'MAILJET_API_KEY': 'a9ed23123ce9013f301d2c6c7b038105',
+          'MAILJET_API_SECRET': 'c1582325f03a0be32490c4af7c012350'
+        },
+      );
+      if (response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 204) {
+        // dynamic responseData = json.decode(response.body);
+        // _logger.i(responseData);
+        return true;
+      } else {
+        _logger.e(
+            'Login failed with status code ${response.statusCode}: ${response.body}');
         return false;
       }
     } catch (e) {
@@ -453,19 +499,19 @@ class ApiService {
   ///
   /// Retourne un [bool] qui indique si la connexion a réussi.
   Future<bool> resetPasswordWithToken(
-    String email,
+    String token,
     String newPassword, {
     Client? client,
   }) async {
     try {
       client ??= _httpClient;
-      Uri url = ApiRoutes.route(ApiRoutes.resetPasswordWithoutTokenRoute);
+      Uri url = ApiRoutes.route(
+          ApiRoutes.resetPasswordWithTokenRoute.replaceAll(":token", token));
       Response response = await client.post(
         url,
         body: json.encode(
           {
-            'email': email,
-            'password': newPassword,
+            'password': newPassword
           },
         ),
         headers: {
@@ -479,7 +525,8 @@ class ApiService {
         _logger.i(responseData);
         return true;
       } else {
-        _logger.e('Login failed with status code ${response.statusCode}');
+        _logger.e(
+            'Login failed with status code ${response.statusCode}: ${response.body}');
         return false;
       }
     } catch (e) {
@@ -526,7 +573,8 @@ class ApiService {
       if (response.statusCode == 200 || response.statusCode == 202) {
         return (response.body);
       } else {
-        _logger.e("calendarValues failed with status code ${response.statusCode}");
+        _logger.e(
+            "calendarValues failed with status code ${response.statusCode}: ${response.body}");
         return "false";
       }
     } catch (e) {
